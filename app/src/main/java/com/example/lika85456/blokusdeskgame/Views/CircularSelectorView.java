0d243ce365f258ee209c0c41248153d902e6a7fb
lika85456 <lika85456@post.cz>
2018-03-21 13:24:38 +0100

    update
    Adding circular selector.
    new TODO - complete dispatchDraw drawing the graphics of the selector

---
+++ app/src/main/java/com/example/lika85456/blokusdeskgame/Views/CircularSelectorView.java
@@ -0,0 +1,48 @@
+package com.example.lika85456.blokusdeskgame.Views;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.example.lika85456.blokusdeskgame.R;
+
+/**
+ * Created by lika85456 on 21.03.2018.
+ */
+
+public class CircularSelectorView extends View {
+
+    public int width, height;
+
+    public CircularSelectorView(Context context) {
+        super(context);
+        this.setBackgroundResource(R.drawable.circular_selector_background);
+
+    }
+
+    public void onMeasure(int w, int h) {
+        width = MeasureSpec.getSize(w);
+        height = MeasureSpec.getSize(h);
+        this.setMeasuredDimension(width, height);
+    }
+
+    @Override
+    public void dispatchTouchEvent(MotionEvent event) {
+
+    }
+
+    @Override
+    public void dispatchDraw(Canvas canvas) {
+        //TODO CLEANUP
+        super.dispatchDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(0xCCC);
+        canvas.drawArc(new RectF(0, 0, width, height), -30, 60, true, paint);
+    }
+
+
+}
